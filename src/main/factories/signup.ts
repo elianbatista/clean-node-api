7e3741e5f241fb3a13c06f25d9bac020d639227a
Elian Batista <elianbatista2000@gmail.com>
2021-02-04 20:07:47 -0300

    refactor: change folder name

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,6 @@
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
-import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account/account'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 
